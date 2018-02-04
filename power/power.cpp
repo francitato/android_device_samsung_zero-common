87268f80f7a6dd4a6410d6596e28964c0c4da575
enesuzun2002 <4lukankalar@gmail.com>
2018-02-04 14:47:48 +0000

    Revert "Fix Bootloop thanks @lukas0610"
    This reverts commit 00f0d313c07a4c60ffd7a3d4ac97cbb33edab899.

--- power/power.cpp
+++ power/power.cpp
@@ -76,7 +76,6 @@ static int power_open(const hw_module_t *module, const char *name, hw_device_t *
 
 				instance = dev;
 				power = container_of(dev, struct sec_power_module, base);
-                pthread_mutex_init(&power->lock,NULL);
 			} else {
 				retval = -ENOMEM;
 			}
@@ -516,7 +515,7 @@ struct sec_power_module HAL_MODULE_INFO_SYM = {
 		.setInteractive = power_set_interactive,
 	},
 
-	.lock = nullptr,
+	.lock = PTHREAD_MUTEX_INITIALIZER,
 
 	.profile = {
 		.current = PROFILE_BALANCED,
